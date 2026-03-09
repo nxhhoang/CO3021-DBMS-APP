a7ea2592a512069e5b1b2ca5cd587ce10607ff8a
lb <buihuuloi2004@gmail.com>
2026-03-09 12:45:36 +0700

    initial

--- BACKEND/dist/middlewares/sample.middlewares.js
+++ BACKEND/dist/middlewares/sample.middlewares.js
@@ -1,8 +1,15 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.createSampleValidator = void 0;
+exports.accessTokenValidator = exports.createSampleValidator = void 0;
 const express_validator_1 = require("express-validator");
 const validation_1 = require("../utils/validation");
+const commons_1 = require("../utils/commons");
+const Errors_1 = require("../models/Errors");
+const httpStatus_1 = __importDefault(require("../constants/httpStatus"));
+const messages_1 = require("../constants/messages");
 exports.createSampleValidator = (0, validation_1.validate)((0, express_validator_1.checkSchema)({
     name: {
         notEmpty: {
@@ -25,3 +32,21 @@ exports.createSampleValidator = (0, validation_1.validate)((0, express_validator
         trim: true
     }
 }, ['body']));
+const accessTokenValidator = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return next(new Errors_1.ErrorWithStatus({
+            message: messages_1.USERS_MESSAGES.ACCESS_TOKEN_IS_REQUIRED,
+            status: httpStatus_1.default.UNAUTHORIZED
+        }));
+    }
+    const access_token = authHeader.split(' ')[1];
+    try {
+        await (0, commons_1.verifyAccessToken)(access_token, req);
+        next();
+    }
+    catch (error) {
+        next(error);
+    }
+};
+exports.accessTokenValidator = accessTokenValidator;
--- BACKEND/eslint.config.mjs
+++ BACKEND/eslint.config.mjs
@@ -1,5 +1,5 @@
-import js from '@eslint/js'
 import eslintPluginPrettier from 'eslint-plugin-prettier'
+import js from '@eslint/js'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 import { defineConfig } from 'eslint/config'
@@ -12,7 +12,7 @@ export default defineConfig([
     files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
     plugins: { js },
     extends: ['js/recommended'],
-    languageOptions: { globals: globals.browser }
+    languageOptions: { globals: globals.node }
   },
   tseslint.configs.recommended,
   {
